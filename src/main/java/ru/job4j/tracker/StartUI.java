f9d91c417fe782677d3d9c59428767a14f683e8e
raiko <raikobob@yandex.ru>
2022-03-07 17:21:18 +0300

    Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime dateTime = item.getCreated();
+        String s = dateTime.format(formatter);
+        System.out.println(s);
+    }
+}
