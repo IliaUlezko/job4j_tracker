643da1455d1e20ccb42b9ed98149769c65ad64a1
raiko <raikobob@yandex.ru>
2022-03-08 21:16:17 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
+
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
@@ -21,5 +24,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                rsl[i] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                result[count] = item;
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
