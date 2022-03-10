175a247c17b6befc91696e556b43b8b2205bf5ed
raiko <raikobob@yandex.ru>
2022-03-10 18:18:53 +0300

    toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,10 +6,11 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("Mouse", 5);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         LocalDateTime dateTime = item.getCreated();
         String s = dateTime.format(formatter);
         System.out.println(s);
+        System.out.println(item);
     }
 }
