2c57cc83270c3ce110b7a23c02b1ea116867c4ba
raiko <raikobob@yandex.ru>
2022-03-13 10:43:59 +0300

    Реализация класса StartUI. Добавление заявки

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,6 +16,7 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
+                System.out.println("Добавлена заявка: " + item);
             } else if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
             } else {
