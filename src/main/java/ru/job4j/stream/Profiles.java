c1060ff5adc147d6b1a6199ad605da7dc29e1d56
raiko <raikobob@yandex.ru>
2022-04-20 17:28:12 +0300

    1

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile::getAddress)
+                .map(profile -> profile.getAddress())
                 .collect(Collectors.toList());
     }
 
