5e4e36f48b6519ac70cbf56a88fad15019222895
IliaUlezko <raikobob@yandex.ru>
2024-02-15 11:32:36 +0300

    PriorityQueue.

--- src/main/java/ru/job4j/queue/TaskByPositionAsc.java
+++ src/main/java/ru/job4j/queue/TaskByPositionAsc.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class TaskByPositionAsc implements Comparator<Task> {
     @Override
-    public int compare(Task o1, Task o2) {
-        return Integer.compare(o1.position().ordinal(), o2.position().ordinal());
+    public int compare(Task first, Task second) {
+        return first.position().compareTo(second.position());
     }
 }
